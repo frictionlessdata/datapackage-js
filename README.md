6bd0a515753371ef010e9337bcf2535d9bc9b09c
Rufus Pollock <rufus.pollock@okfn.org>
2016-06-15 17:56:59 +0100

    [all][xl]: extract basic data package js lib from datapackage-render-js.
    * Basic DataPackage object plus Resource
    * Extracted as of https://github.com/frictionlessdata/datapackage-render-js/tree/0d05d7369245820e75b0683756c2518dede3ae3f

--- README.md
+++ README.md
@@ -3,3 +3,79 @@
 [![Gitter](https://img.shields.io/gitter/room/frictionlessdata/chat.svg)](https://gitter.im/frictionlessdata/chat)
 
 A model for working with Data Packages in Node and the browser.
+
+## Install
+
+**npm coming soon**.
+
+For now clone from github!
+
+<!--
+[![NPM](https://nodei.co/npm/datapackage.png)](https://nodei.co/npm/datapackage-render/)
+
+```
+npm install datapackage-render
+```
+-->
+
+# Usage
+
+```
+var datapacakge = require('datapackage');
+
+// or instantiate with a path and load
+var dp = new datapackage.DataPackage('/path/on/disk/to/package/')
+dp.load()
+  .then(function() {
+    // 'abc'
+    console.log(dp.data.name);
+  });
+
+
+// you can also load direct from JSON object
+var dpJson = {
+  'name': 'abc'
+};
+var dp = new datapackage.DataPackage(dpJson);
+```
+
+### Resources
+
+```
+var dp = a data package with some resources
+
+// Access the Data Pacakge data resources
+var resource = dp.resources[0]
+
+// will return the full path (url or on disk for the resource)
+resource.fullPath()
+
+// get the raw data file stream
+resource.rawStream()
+
+// get the data stream as set of JS objects (one for each row)
+// this only works for tabular data at present
+// data is automatically typecast
+resource.stream()
+```
+
+### csvToSTream
+
+Convenience function to load and convert a CSV to a stream using a [JSON Table Schema][jts] and [CSV Dialect description][dialect].
+
+[jts]: http://frictionlessdata.io/guides/json-table-schema/
+[dialect]: http://dataprotocols.org/csv-dialect/
+
+```
+// returns a Node **object** stream
+// the csv dialect is optional
+var stream = datapackage.csvToStream(rawDataStream, jsonTableSchema, csvDialect)
+
+stream.on('readable', function() {
+  while(row = stream.read()) {
+    // something like {'col-1': 'abc', 'col-2': 2, ...}
+    console.log(row);
+  }
+});
+```
+
---
+++ test/datapackage.js
@@ -0,0 +1,285 @@
+var assert = require('assert')
+  , spec = require('../index.js')
+  , stream = require('stream')
+  , fs = require('fs')
+  ;
+
+var dp1 = {
+  "name": "abc",
+  "resources": [
+    {
+      "name": "random",
+      "format": "csv",
+      "path": "test/fixtures/dp1/data.csv",
+      "schema": {
+        "fields": [
+          {
+            "name": "name",
+            "type": "string"
+          },
+          {
+            "name": "size",
+            "type": "integer"
+          }
+        ]
+      }
+    }
+  ],
+  "views": [
+    {
+      "type": "vegalite",
+      "spec": {
+        "data": {
+          "resource": "random"
+        },
+        "mark": "bar",
+        "encoding": {
+          "x": {"field": "name", "type": "ordinal"},
+          "y": {"field": "size", "type": "quantitative"}
+        }
+      }
+    }
+  ]
+};
+
+describe('DataPackage', function() {
+  it('instantiates', function() {
+    var dp = new spec.DataPackage();
+  });
+
+  it('instantiates with string', function() {
+    var dp = new spec.DataPackage('abc');
+    assert.equal(dp.path, 'abc');
+  });
+  
+  it('instantiates with object', function() {
+    var dp = new spec.DataPackage(dp1);
+    assert.deepEqual(dp.data, dp1);
+  });
+
+  it('loads', function(done) {
+    var dp = new spec.DataPackage('test/fixtures/dp1');
+    dp.load()
+      .then(function() {
+        assert.equal(dp.data.name, 'abc');
+        assert.equal(dp.resources.length, 1);
+        assert.equal(dp.resources[0].fullPath(), 'test/fixtures/dp1/data.csv');
+        done();
+      });
+  });
+
+});
+
+describe('Resource', function() {
+  var resource = {
+    "path": "test/fixtures/dp1/data.csv"
+  }
+  it('instantiates', function() {
+    var res = new spec.Resource(resource);
+    assert.equal(res.data, resource);
+    assert.equal(res.base, '');
+  });
+  it('fullPath works', function() {
+    var res = new spec.Resource(resource, 'abc');
+    assert.equal(res.base, 'abc');
+    assert.equal(res.fullPath(), 'abc/test/fixtures/dp1/data.csv');
+  });
+  it('objects works', function(done) {
+    var res = new spec.Resource(resource);
+    res.objects()
+      .then(function(output) {
+        assert.equal(output.length, 3);
+        assert.equal(output[0].size, "100");
+        done();
+      });
+  });
+  it('stream works', function(done) {
+    var res = new spec.Resource(resource);
+    spec.objectStreamToArray(res.stream()).
+      then(function(output) { 
+        assert.equal(output.length, 3);
+        assert.strictEqual(output[0].size, "100");
+        done();
+      });
+  });
+  it('stream works with jts', function(done) {
+    var res = new spec.Resource(dp1.resources[0]);
+    spec.objectStreamToArray(res.stream()).
+      then(function(output) { 
+        assert.equal(output.length, 3);
+        assert.strictEqual(output[0].size, 100);
+        done();
+      });
+  });
+});
+
+function makeStream(text) {
+  var s = new stream.Readable();
+  s.push(text);
+  s.push(null);
+  return s;
+}
+
+describe('csvToStream', function() {
+  it('casting works', function(done) {
+    var dp = new spec.DataPackage(dp1);
+    var stream = spec.csvToStream(dp.resources[0].rawStream(), dp.resources[0].data.schema);
+    spec.objectStreamToArray(stream).
+      then(function(output) { 
+        assert.equal(output.length, 3);
+        assert.strictEqual(output[0].size, 100);
+        done();
+      });
+  });
+
+  var schema = fs.readFileSync("test/fixtures/types-test/schema.json", "utf8")
+  var jsonContent = JSON.parse(schema)
+  var dp = new spec.DataPackage(jsonContent);
+
+  it('parse works for strings', function(done) {
+    var stream = spec.csvToStream(dp.resources[0].rawStream(), dp.resources[0].data.schema);
+    spec.objectStreamToArray(stream).
+      then(function(output) {
+        assert.strictEqual(typeof output[0].string, 'string');
+        assert.strictEqual(output[0].string, 'Word');
+        assert.strictEqual(output[1].string, 'Two words');
+        assert.strictEqual(output[2].string, 'test@mail.com');
+        assert.strictEqual(output[3].string, 'http://www.testwebsite.com');
+        assert.strictEqual(output[4].string, 'CAPITALIZED');
+        assert.strictEqual(output[5].string, 'list,of,words');
+        assert.strictEqual(output[6].string, 'text\\nwith\\nnew\\nlines');
+        assert.strictEqual(output[7].string, ' ');
+        assert.strictEqual(output[8].string, '');
+        assert.strictEqual(output[9].string, '0123456789');
+        assert.strictEqual(output[10].string, '!@#$%^&*()_+{}[]:;"\\|<.>');
+        done();
+      });
+  });
+  it('parse works for numbers', function(done) {
+    var stream = spec.csvToStream(dp.resources[0].rawStream(), dp.resources[0].data.schema);
+    spec.objectStreamToArray(stream).
+      then(function(output) {
+        assert.strictEqual(typeof output[0].number, 'number');
+        assert.strictEqual(output[0].number, 0);
+        assert.strictEqual(output[1].number, -100.58);
+        assert.strictEqual(output[2].number, -1);
+        assert.strictEqual(output[3].number, 3.14);
+        assert.strictEqual(output[4].number, 0.00001);
+        assert.strictEqual(output[5].number, 1);
+        assert.strictEqual(output[6].number, 1.0001);
+        assert.strictEqual(output[7].number, -0.4);
+        assert.strictEqual(output[8].number, -0);
+        assert.strictEqual(output[9].number, 123456789);
+        assert.strictEqual(output[10].number, 17);
+        done();
+      });
+  });
+  it('parse works for integers', function(done) {
+    var stream = spec.csvToStream(dp.resources[0].rawStream(), dp.resources[0].data.schema);
+    spec.objectStreamToArray(stream).
+      then(function(output) {
+        assert.strictEqual(typeof output[0].integer, 'number');
+        assert.strictEqual(output[0].integer, 0);
+        assert.strictEqual(output[1].integer, 5);
+        assert.strictEqual(output[2].integer, 1000000);
+        assert.strictEqual(output[3].integer, -1000);
+        assert.strictEqual(output[4].integer, 15);
+        assert.strictEqual(output[5].integer, 12);
+        assert.strictEqual(output[6].integer, 2);
+        assert.strictEqual(output[7].integer, 123456);
+        assert.strictEqual(output[8].integer, 3);
+        assert.strictEqual(output[9].integer, 1);
+        assert.strictEqual(output[10].integer, 4);
+        done();
+      });
+  });
+  it('parse works for booleans', function(done) {
+    var stream = spec.csvToStream(dp.resources[0].rawStream(), dp.resources[0].data.schema);
+    spec.objectStreamToArray(stream).
+      then(function(output) {
+        assert.strictEqual(typeof output[0].boolean, 'boolean');
+        assert.strictEqual(output[0].boolean, true);
+        assert.strictEqual(output[1].boolean, false);
+        assert.strictEqual(output[2].boolean, true);
+        assert.strictEqual(output[3].boolean, false);
+        assert.strictEqual(output[4].boolean, false);
+        assert.strictEqual(output[5].boolean, true);
+        assert.strictEqual(output[6].boolean, true);
+        assert.strictEqual(output[7].boolean, false);
+        assert.strictEqual(output[8].boolean, false);
+        assert.strictEqual(output[9].boolean, true);
+        assert.strictEqual(output[9].boolean, true);
+        done();
+      });
+  });
+  it('parse works for dates', function(done) {
+    var stream = spec.csvToStream(dp.resources[0].rawStream(), dp.resources[0].data.schema);
+    spec.objectStreamToArray(stream).
+      then(function(output) {
+        assert.strictEqual(typeof output[0].date, 'number');
+        assert.strictEqual(output[0].date, 1463788800000);
+        assert.strictEqual(output[1].date, 1462060800000);
+        // assert.strictEqual(output[2].date, 1463827423000);
+        // assert.strictEqual(output[3].date, 1463947200000);
+        // assert.strictEqual(output[4].date, 1461355200000);
+        // assert.strictEqual(output[5].date, 1472673600000);
+        // assert.strictEqual(output[6].date, 1451592000000);
+        // assert.strictEqual(output[7].date, 1462219200000);
+        // assert.strictEqual(output[8].date, 1454616000000);
+        // assert.strictEqual(output[9].date, 1451764800000);
+        // assert.strictEqual(output[10].date, 997902000000);
+        done();
+      });
+  });
+});
+
+
+describe('csv dialect support', function() {
+  it('works with delimiter', function(done) {
+    var content = fs.createReadStream('test/fixtures/csv-dialects/data-del.csv')
+    var dp = new spec.DataPackage(dp1);
+    var stream = spec.csvToStream(content, dp.resources[0].data.schema, {delimiter: '\t'});
+    spec.objectStreamToArray(stream).
+      then(function(output) {
+        assert.equal(output.length, 3);
+        assert.strictEqual(output[2].info, "test,for,delimiter");
+        done();
+      });
+  });
+  it('works with quoteChar', function(done) {
+    var content = fs.createReadStream('test/fixtures/csv-dialects/data-qc.csv')
+    var dp = new spec.DataPackage(dp1);
+    var stream = spec.csvToStream(content, dp.resources[0].data.schema, {quoteChar: "'"});
+    spec.objectStreamToArray(stream).
+      then(function(output) {
+        assert.equal(output.length, 3);
+        assert.strictEqual(output[1].info,'U,S,A');
+        done();
+      });
+  });
+  it('works with doubleQuote', function(done) {
+    var content = fs.createReadStream('test/fixtures/csv-dialects/data-dq.csv')
+    var dp = new spec.DataPackage(dp1);
+    var stream = spec.csvToStream(content, dp.resources[0].data.schema, {doubleQuote: '"'});
+    spec.objectStreamToArray(stream).
+      then(function(output) {
+        assert.equal(output.length, 3);
+        assert.strictEqual(output[2].info, 'no "info for this"');
+        done();
+      });
+  });
+  it('works with all csv dialects', function(done) {
+    var content = fs.createReadStream('test/fixtures/csv-dialects/data-all.csv')
+    var dp = new spec.DataPackage(dp1);
+    var stream = spec.csvToStream(content, dp.resources[0].data.schema, {delimiter: '\t', quoteChar: "'", doubleQuote: '"'});
+    spec.objectStreamToArray(stream).
+      then(function(output) {
+        assert.equal(output.length, 3);
+        assert.strictEqual(output[0].size, 100);
+        assert.strictEqual(output[1].info, 'U	S	A');
+        assert.strictEqual(output[2].info, '"no info"');
+        done();
+      });
+  });
+});
+
