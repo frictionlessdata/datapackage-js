4601ef56ff8d8737732e139d1282211605b7bace
Damjan Velickovski <damjan91@gmail.com>
2016-11-29 03:01:24 +0100

    Initial Resource implementation

--- src/resource.js
+++ src/resource.js
@@ -1,6 +1,58 @@
 'use strict'
 
-export default () => {
+import url from 'url'
+import jts from 'jsontableschema'
+import _ from 'lodash'
 
+export default class Resource {
+
+  constructor(descriptor) {
+    this._descriptor = descriptor
+    this.REMOTE_PROTOCOLS = ['http:', 'https:', 'ftp:', 'ftps:']
+  }
+
+  getDescriptor() {
+    return this._descriptor
+  }
+
+  getName() {
+    return this.getDescriptor()['name']
+  }
+
+  getType() {
+    const resourceField = this._getResourceField()
+
+    if (resourceField === 'data') {
+      return 'inline'
+    }
+
+    const protocol = url.parse(this.getDescriptor()[resourceField])['protocol']
+    if (_.includes(this.REMOTE_PROTOCOLS, protocol)) {
+      return 'remote'
+    } else {
+      return 'local'
+    }
+
+  }
+
+  getSource() {
+    return this.getDescriptor()[this._getResourceField()]
+  }
+
+  getTable() {
+    const resourceField = this._getResourceField()
+    return new jts.Table(this.getDescriptor()['schema'],
+                         this.getDescriptor()[resourceField])
+  }
+
+  _getResourceField() {
+    const inlineData = this.getDescriptor()['data']
+    const path = this.getDescriptor()['path']
+
+    if (inlineData) {
+      return 'data'
+    } else {
+      return 'path'
+    }
+  }
 }
-
