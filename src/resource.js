34f6de25e4f5c9c1ec462f2bbe63129e3f48c2a2
Damjan Velickovski <damjan91@gmail.com>
2016-12-06 06:22:04 +0100

    Added Resource class (#10)
    * Remove tests
    
    * Copy jsontableschema-js skeleton
    
    * Add .idea/
    
    * Use jsontableschema-js scripts and deps
    
    * Update template, add badges, add note about the API changes
    
    * Add testing data previously found in test/fixtures #6366ce930b9085081b3ff275fb3af65a0829cff6
    
    * Stub code for passing the tests
    
    * Add babel and mocha configs
    
    * Stub test
    
    * Stub index for the build scripts
    
    * Add lodash as dependency
    
    * Add jts and nock as dependencies
    
    * Initial Resource implementation
    
    * Initial tests for the Resource implementation
    
    * Use js getters
    
    * Rename _getResourceField to _getSourceKey
    
    * Use the source getter in get table
    
    * Add transform-async-to-generator
    
    * Use await instead of then on resource.table
    
    * Add docstrings
    
    * Add babel-plugin-transform-async-to-generator
    
    * Add tests that work on dp1 from data/
    
    * table getter discards errors and resolves in null, make sourceKey getter
    
    * Add test to make resource.table resolves in null
    
    * Add test for resource.table with csv file
    
    * Move test data to test directory
    
    * Move test data to data/ directory
    
    * Rename sourceKey to _sourceKey, reject rejected jts.Table with null
    
    * Test on actual data from csv file, expect rejected table to be null
    
    * Add simple caching for jts.Table and make it resolve to null on errors
    
    * Expect table promise to resolve to null on errors

--- src/resource.js
+++ src/resource.js
@@ -1,6 +1,108 @@
 'use strict'
 
-export default () => {
+import url from 'url'
+import jts from 'jsontableschema'
+import _ from 'lodash'
 
+/**
+ * Base class for all Data Package's resource types.
+ *
+ * @returns {Resource}
+ */
+export default class Resource {
+
+  constructor(descriptor) {
+    this._descriptor = descriptor
+    this.REMOTE_PROTOCOLS = ['http:', 'https:', 'ftp:', 'ftps:']
+  }
+
+  /**
+   * Returns the descriptor that was used to initialize the class.
+   *
+   * @returns {*}
+   */
+  get descriptor() {
+    return this._descriptor
+  }
+
+  /**
+   * Returns the name in the descriptor.
+   *
+   * @returns {String}
+   */
+  get name() {
+    return this.descriptor['name']
+  }
+
+  /**
+   * Returns the location of the data. Possible values are 'inline', 'remote'
+   * and 'local'.
+   *
+   * @returns {String}
+   */
+  get type() {
+    const resourceField = this._sourceKey
+
+    if (resourceField === 'data') {
+      return 'inline'
+    }
+
+    const protocol = url.parse(this.descriptor[resourceField])['protocol']
+    if (_.includes(this.REMOTE_PROTOCOLS, protocol)) {
+      return 'remote'
+    } else {
+      return 'local'
+    }
+
+  }
+
+  /**
+   * Returns the path where data is located or
+   * if the data is inline it returns the actual data
+   *
+   * @returns {String|Array|Object}
+   */
+  get source() {
+    return this.descriptor[this._sourceKey]
+  }
+
+  /**
+   * Initializes the jsontableschema.Table class with the provided descriptor.
+   * If data is not tabular or schema is not defined or not valid the promise
+   * resolves to `null`
+   *
+   * See https://github.com/frictionlessdata/jsontableschema-js#table
+   *
+   * @returns {Promise}
+   */
+  get table() {
+    if (!this._table) {
+      this._table = new Promise((resolve) => {
+        new jts.Table(this.descriptor['schema'], this.source).then((res) => {
+          resolve(res)
+        }).catch(() => {
+          resolve(null)
+        })
+      })
+    }
+    return this._table
+  }
+
+  /**
+   * Private function used to identify if the descriptor contains inline data
+   * or provides a path for the data.
+   *
+   * @returns {String}
+   * @private
+   */
+  get _sourceKey() {
+    const inlineData = this.descriptor['data']
+    const path = this.descriptor['path']
+
+    if (inlineData) {
+      return 'data'
+    } else {
+      return 'path'
+    }
+  }
 }
-
--- test/resource.js
+++ test/resource.js
@@ -1,10 +1,163 @@
 'use strict'
 
+import 'babel-polyfill'
 import { assert } from 'chai'
+import jts from 'jsontableschema'
 import Resource from '../src/resource'
+import _ from 'lodash'
+
+import dp1 from '../data/dp1/datapackage.json'
 
 describe('Resource', () => {
-  it('Should export resource', () => {
-    assert(Resource, 'resource variable is empty')
+
+  it('returns expected descriptor', () => {
+    const resourceDesc = {
+      'name': 'foo',
+      'url': 'http://someplace.com/foo.json',
+      'path': 'foo.json',
+      'data': { 'foo': 'bar' }
+    }
+    let resource = new Resource(resourceDesc)
+    assert(resource.descriptor === resourceDesc, 'Invalid descriptor')
+  })
+
+  it('contains no source by default', () => {
+    const resourceDesc = {}
+    let resource = new Resource(resourceDesc)
+    assert(resource.source === undefined, 'Invalid source')
+  })
+
+  it('returns the expected test data', () => {
+    const resourceDesc = {
+      'data': 'foo'
+    }
+    let resource = new Resource(resourceDesc)
+    assert(resource.source === 'foo', 'Invalid source')
+  })
+
+  it('returns the expected name', () => {
+    const resourceDesc = {
+      'name': 'FooBar'
+    }
+    let resource = new Resource(resourceDesc)
+    assert(resource.name === resourceDesc['name'], 'Invalid name')
+  })
+
+  it('recognizes that data type is local', () => {
+    const resouceDesc = {
+      'path': 'foo/bar.txt'
+    }
+    let resource = new Resource(resouceDesc)
+    assert(resource.type === 'local', 'Invalid data type')
+  })
+
+  it('recognizes that data type is remote', () => {
+    const resouceDesc = {
+      'path': 'http://www.foo.org/bar.txt'
+    }
+    let resource = new Resource(resouceDesc)
+    assert(resource.type === 'remote', 'Invalid data type')
+  })
+
+  it('recognizes that data is inline', () => {
+    const resouceDesc = {
+      'data': 'foo, bar'
+    }
+    let resource = new Resource(resouceDesc)
+    assert(resource.type === 'inline', 'Inline data not found')
+  })
+
+  it('table getter returns jts.Table', async() => {
+    const resourceDesc = {
+      'data': 'http://foofoo.org/data.csv',
+      'schema': {
+        'fields': [
+          { 'name': 'barfoo' }
+        ]
+      }
+    }
+
+    let resource = new Resource(resourceDesc)
+    let table = await resource.table
+    assert(table instanceof jts.Table,
+           'Returned object is not instance of Table')
+  })
+
+  it('table getter returns null if jsontableschama.Table throws an error',
+     async() => {
+       const resourceDesc = {
+         'data': 'http://foofoo.org/data.csv',
+       }
+       let resource = new Resource(resourceDesc)
+       let table = await resource.table
+       assert(table === null, 'Returned value not null')
+     })
+
+  describe('Tests with dp1 from data', () => {
+    let dpResources = []
+
+    beforeEach(() => {
+      _.forEach(dp1.resources, (res) => {
+        dpResources.push(res)
+      })
+    })
+
+    it('loads the resource descriptor', () => {
+      _.forEach(dpResources, (res) => {
+        let resource = new Resource(res)
+        assert(resource.descriptor === res, 'Wrong descriptor')
+      })
+    })
+
+    it('returns the correct name', () => {
+      _.forEach(dpResources, (res) => {
+        let resource = new Resource(res)
+        assert(resource.name == res.name)
+      })
+    })
+
+    it('returns the correct source', () => {
+      _.forEach(dpResources, (res) => {
+        let resource = new Resource(res)
+        assert(resource.source === res.path)
+      })
+    })
+
+    it('initialize jsontableschema.Table with csv file',
+       async(done) => {
+         let resource = new Resource({
+           "name": "dp1",
+           "format": "csv",
+           "path": "data/dp1/data.csv",
+           "schema": {
+             "fields": [
+               {
+                 "name": "name",
+                 "type": "string"
+               },
+               {
+                 "name": "size",
+                 "type": "integer"
+               }
+             ]
+           }
+         })
+         try {
+           let table = await resource.table
+           let data = await table.read(false, false, 1)
+           if (data.toString() === "gb,100") {
+             done()
+           } else {
+             done(Error('Invalid data'))
+           }
+         } catch (err) {
+           done(Error(`Table not initialized, resource.table returned: ${err}`))
+         }
+       })
+
+    it('returns \'local\' type', () => {
+      let resource = new Resource(dp1.resources[0])
+      assert(resource.type === 'local', 'Incorrect type for datapackage')
+    })
   })
 })
