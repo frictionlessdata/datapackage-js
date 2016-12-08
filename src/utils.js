d35d1b765b040939db2758c84364f33a3fd268e6
Damjan Velickovski <damjan91@gmail.com>
2016-12-08 10:50:10 +0100

    Add function for parsing tv4 errors, code lint

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,17 @@
 import parse from 'csv-parse';
 import 'isomorphic-fetch'
+import _ from 'lodash'
 
 const isBrowser = typeof window !== 'undefined';
 
 let fs
 if (!isBrowser) {
-  fs = require('fs');
+  fs = require('fs')
 }
 
 class Utils {
   static isRemoteURL(path) {
-    return path.match(/\w+:\/\/.+/);
+    return path.match(/\w+:\/\/.+/)
   }
 
   static get isBrowser() {
@@ -23,11 +24,11 @@ class Utils {
         .then((response) => {
           if (!response.ok) {
             console.log(response.text())
-            throw new Error('Bad response from server');
+            throw new Error('Bad response from server')
           }
 
-          return response.text();
-        });
+          return response.text()
+        })
     }
 
     function _readFile(path) {
@@ -35,35 +36,50 @@ class Utils {
       return new Promise((resolve, reject) => {
         fs.readFile(path, 'utf8', (err, data) => {
           if (err) {
-            reject(err);
+            reject(err)
           } else {
-            resolve(data);
+            resolve(data)
           }
-        });
-      });
+        })
+      })
     }
 
-    let result;
+    let result
 
     if (isBrowser || Utils.isRemoteURL(pathOrURL)) {
-      result = _readURL(pathOrURL);
+      result = _readURL(pathOrURL)
     } else {
-      result = _readFile(pathOrURL);
+      result = _readFile(pathOrURL)
     }
 
-    return result;
+    return result
+  }
+
+  static tv4Errors(values) {
+    const result = []
+    _.forEach(values, error => {
+      let errorMessage = error.message
+      if (error.dataPath) {
+        errorMessage += ` in "${error.dataPath}"`
+      }
+      if (error.schemaPath) {
+        errorMessage += ` schema path: "${error.schemaPath}"`
+      }
+      result.push(errorMessage)
+    })
+    return result
   }
 
   static _csvParse(text) {
     return new Promise((resolve, reject) => {
       parse(text, { columns: true }, (err, output) => {
         if (err) {
-          reject(err);
+          reject(err)
         } else {
-          resolve(output);
+          resolve(output)
         }
-      });
-    });
+      })
+    })
   }
 }
 
