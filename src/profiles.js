107ff621eb643ec2d5a6d4c317280cdce9d9d207
Damjan Velickovski <damjan91@gmail.com>
2016-12-13 14:43:58 +0100

    Make constructor async

--- src/profiles.js
+++ src/profiles.js
@@ -14,25 +14,38 @@ if (!Utils.isBrowser) {
 class Profiles {
 
   constructor(remote = false) {
-    if (remote || Utils.isBrowser) {
-      this._registry = this._loadRegistry(DEFAULT_REMOTE_PATH)
-      this._base_path = this._loadBasePath(DEFAULT_REMOTE_PATH)
-    } else {
-      this._registry = this._loadRegistry(DEFAULT_LOCAL_PATH)
-      this._base_path = this._loadBasePath(DEFAULT_LOCAL_PATH)
-    }
-  }
+    const self = this
+        , PATH = remote || Utils.isBrowser ? DEFAULT_REMOTE_PATH :
+                 DEFAULT_LOCAL_PATH
 
-  // ------ Profiles methods -------
+    return new Promise((resolve, reject) => {
+      const promises = []
+      promises.push(this._loadRegistry(PATH))
+      promises.push(this._loadBasePath(PATH))
+
+      Promise.all(promises).then(values => {
+        self._registry = values[0]
+        self._base_path = values[1]
+        this._getProfiles().then(profiles => {
+          self._allProfiles = profiles
+          resolve(self)
+        }).catch(err => {
+          reject(err)
+        })
+      }).catch(err => {
+        reject(err)
+      })
+    })
+  }
 
   /**
    * Retrieve a profile by id.
    *
-   * @param profile
+   * @param {String} [profile='base']
    * @return {Promise}
    */
-  retrieve(profile) {
-    return this._getProfile(profile)
+  retrieve(profile = 'base') {
+    return this._allProfiles[profile]
   }
 
   /**
@@ -41,58 +54,38 @@ class Profiles {
    *
    * @param {String|JSON} descriptor The descriptor that needs to be validated
    * @param {String|JSON} profile Schema to validate against
-   * @return {Promise} Resolves `true` or array of strings which explain the errors.
+   * @return {Promise} Resolves `true` or array of strings which explain the
+   *   errors.
    */
   validate(descriptor, profile = 'base') {
+    function _tv4validation(data, schema) {
+      const validate = tv4.validateMultiple(data, schema)
+      if (validate.valid) {
+        return true
+      }
+
+      return Utils.errorsToStringArray(validate.errors)
+    }
+
     let json = descriptor
 
     if (typeof descriptor === 'string') {
       const lint = jsonlint(descriptor)
-
       if (lint.error) {
-        return new Promise((resolve, reject) => {
-          reject(Error(lint.error.toString()))
-        })
+        return Utils.errorsToStringArray(new Error(lint.error))
       }
 
       json = JSON.parse(descriptor)
     }
 
     if (_.isObject(profile) && !_.isArray(profile) && !_.isFunction(profile)) {
-      return new Promise((resolve, reject) => {
-        const valid = tv4.validateMultiple(json, profile)
-        if (valid) {
-          resolve(valid)
-        }
-        reject(Error(tv4.error))
-      })
+      return _tv4validation(json, profile)
     }
 
-    return new Promise((resolve, reject) => {
-      this.retrieve(profile).then(schema => {
-        const validation = tv4.validateMultiple(json, schema)
-        if (!validation.errors) {
-          resolve(true)
-        }
-        reject(Utils.tv4Errors(validation.errors))
-      }).catch(err => {
-        reject(err)
-      })
-    })
+    return _tv4validation(json, this.retrieve(profile))
   }
 
-  // ------ Private methods related with registry ------
-
-  /**
-   * Returns all _profiles.
-   *
-   * @return {JSON}
-   * @private
-   */
-  get _profiles() {
-    // Returns the available _profiles' metadata.
-    return this._registry
-  }
+  // ------ Private methods  -------
 
   /**
    * Returns the base path.
@@ -101,22 +94,7 @@ class Profiles {
    * @private
    */
   get _basePath() {
-    // If there's a Registry cache, returns its absolute base path
     return this._base_path
-  }
-
-  /**
-   * Get a profile by id.
-   *
-   * @param {String} profileId
-   * @return {Promise} Resolves in JSON if found, `undefined` if not found
-   * @private
-   */
-  _getProfile(profileId) {
-    // Return the profile with the specified ID if it exists
-    return this._profiles
-      .then(registry => registry[profileId])
-      .then(profile => this._loadProfile(profile))
   }
 
   /**
@@ -132,7 +110,7 @@ class Profiles {
       .then(registry => Profiles._groupProfilesById(registry))
   }
 
-  /** CHECHEK
+  /**
    * Loads the base path (dirname) of the path.
    *
    * @param pathOrURL
@@ -171,6 +149,34 @@ class Profiles {
   }
 
   /**
+   * Get all profiles.
+   *
+   * @return {Promise}
+   * @private
+   */
+  _getProfiles() {
+    let profiles = {}
+    const getProfilePromises = []
+
+    _.forEach(this._registry, (value, key) => {
+      getProfilePromises.push(this._loadProfile(value)
+                                .then(res => {
+                                  return { [key]: res }
+                                }))
+    })
+
+    return Promise.all(getProfilePromises).then(values => {
+      _.forEach(values, value => {
+        profiles = _.extend(profiles, value)
+      })
+      return profiles
+    }).catch(err => {
+      throw new Error(err)
+    })
+  }
+
+
+  /**
    * Groups the passed _profiles by id and returns the object.
    *
    * @param registry
@@ -189,3 +195,5 @@ class Profiles {
 }
 
 export default Profiles
+
+/* eslint arrow-body-style: "off" */
