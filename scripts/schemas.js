62a3fe9a99d078b23fe154ba1b4918dba63e82ee
roll <eskarev@gmail.com>
2016-12-31 09:46:27 +0300

    replaced registry.csv by registry.json

--- scripts/schemas.js
+++ scripts/schemas.js
@@ -8,7 +8,7 @@ import utils from '../src/utils'
 
 // Main
 
-const registryURL = 'http://schemas.datapackages.org/registry.csv'
+const registryURL = 'http://schemas.datapackages.org/registry.json'
 const schemasDir = path.join(__dirname, '..', 'src', 'schemas')
 const firstArg = process.argv[2]
 
@@ -16,19 +16,19 @@ if (firstArg === 'check') {
   console.log('checking')
   fetch(registryURL).then(res => res.text())
     .then(text => {
-      const local = fs.readFileSync(path.join(schemasDir, 'registry.csv'), 'utf8')
+      const local = fs.readFileSync(path.join(schemasDir, 'registry.json'), 'utf8')
       if (local === text) {
         console.log('Schemas up to date!')
         process.exit(0)
       }
 
-      console.log('New schemas available. Run "npm run update-schemas" to update.')
+      console.log('New schemas available. Run "npm run schemas:update" to update.')
       process.exit(-1)
     })
 } else if (firstArg === 'update') {
   fetch(registryURL).then(res => res.text())
     .then(text => {
-      fs.writeFileSync(path.join(schemasDir, 'registry.csv'), text)
+      fs.writeFileSync(path.join(schemasDir, 'registry.json'), text)
       return utils._csvParse(text)
     }).then(registry => {
       _.forEach(registry, profile => {
