2fd06274480a75ab7d152594df6e9ed8a102f52b
Adam Kariv <adam.kariv@gmail.com>
2017-04-11 20:31:53 +0300

    Modify jsdom setup

--- test/browser/jsdomSetup.js
+++ test/browser/jsdomSetup.js
@@ -4,12 +4,9 @@ import fs from 'fs'
 const testBundle = fs.readFileSync('./dist/datapackage.js', 'utf8')
 
 export default testingClass => {
-  const document = jsdom.jsdom()
+  const element = `<script>${testBundle}</script>`
+  const document = jsdom.jsdom(element)
   const window = document.defaultView
-
-  const scriptEl = document.createElement('script')
-  scriptEl.textContent = testBundle
-  document.body.appendChild(scriptEl)
 
   return window.datapackage[testingClass]
 }
