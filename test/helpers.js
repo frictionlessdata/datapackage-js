18ffd5f1cefd00812a213aa510165d89e132c80b
roll <eskarev@gmail.com>
2017-09-28 12:32:13 +0300

    Added isSafePath tests

--- test/helpers.js
+++ test/helpers.js
@@ -1,12 +1,29 @@
 const {assert} = require('chai')
-const {helpers} = require('../src/helpers')
+const helpers = require('../src/helpers')
 
 
 // Tests
 
-describe('helpers', () => {
+describe.only('helpers', () => {
 
-  it('#isSafePath', async function() {
+  [ // path, isSafe
+    ['data.csv', true],
+    ['data/data.csv', true],
+    ['data/country/data.csv', true],
+    ['data\\data.csv', true],
+    ['data\\country\\data.csv', true],
+    ['../data.csv', false],
+    ['~/data.csv', false],
+    ['~invalid_user/data.csv', false],
+    ['%userprofile%', false],
+    ['%unknown_windows_var%', false],
+    ['$HOME', false],
+    ['$UNKNOWN_VAR', false],
+  ].forEach(test => {
+    const [path, isSafe] = test
+    it(`#isSafePath: ${path} -> ${isSafe}`, async () => {
+      assert.deepEqual(helpers.isSafePath(path), isSafe)
+    })
   })
 
 })
