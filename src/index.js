fafb8f7b65b48c2b55d2bdceea9f335e9a8c8fe6
roll <eskarev@gmail.com>
2017-11-25 14:42:06 +0300

    Fixed profile export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('regenerator-runtime/runtime')
 const {Package} = require('./package')
 const {Resource} = require('./resource')
-const {Profile} = require('./resource')
+const {Profile} = require('./profile')
 const {validate} = require('./validate')
 const {infer} = require('./infer')
 const errors = require('./errors')
